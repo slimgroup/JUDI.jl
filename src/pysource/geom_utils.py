baa72886f05de934fc2a235b1a60d83228f8975a
mloubout <mathias.louboutin@gmail.com>
2020-03-15 16:34:29 -0400

    tweak src

--- src/pysource/geom_utils.py
+++ src/pysource/geom_utils.py
@@ -16,7 +16,7 @@ def src_rec(model, u, src_coords=None, rec_coords=None, wavelet=None, fw=True, n
         if model.is_tti:
             u_n = (u[0].forward, u[1].forward) if fw else (u[0].backward, u[1].backward)
             geom_expr += src.inject(field=u_n[0], expr=src*dt**2/m)
-            geom_expr += src.inject(field=u_n[1], expr=src*dt**2/m)
+            # geom_expr += src.inject(field=u_n[1], expr=src*dt**2/m)
         else:
             u_n = u.forward if fw else u.backward
             geom_expr += src.inject(field=u_n, expr=src*dt**2/m)
@@ -24,7 +24,7 @@ def src_rec(model, u, src_coords=None, rec_coords=None, wavelet=None, fw=True, n
     rcv = None
     if rec_coords is not None:
         rcv = Receiver(name="rcv", grid=model.grid, ntime=nt, coordinates=rec_coords)
-        rec_expr = (u[0] + u[1]) if model.is_tti else u
+        rec_expr = u[0] if model.is_tti else u
         adj_rcv = rcv.interpolate(expr=rec_expr)
         geom_expr += adj_rcv
     return geom_expr, src, rcv
