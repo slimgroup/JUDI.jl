b08c9d6478d2f6f59493317cc647570fbda86d1e
Mathias Louboutin <mathias.louboutin@gmail.com>
2021-03-10 14:49:16 -0500

    fix compiler flag

--- src/pysource/utils.py
+++ src/pysource/utils.py
@@ -87,7 +87,7 @@ def opt_op(model):
     model: Model
         Model structure to know if we are in a TTI model
     """
-    opts = {'openmp': True, 'mpi': False, 'par-collapse-ncores': 2}
+    opts = {'openmp': True, 'par-collapse-ncores': 2}
     # Minimal size temporaries
     if not model.fs:
         try:
