d521276b7cfe9d2ff5b97db78e0a55ef74bfcedb
kerim371 <kerim.khemraev@mail.ru>
2023-04-19 15:51:07 +0300

    Removed .pdf, improved README

---
+++ examples/field_examples/viking_graben_line12/download_data.sh
@@ -0,0 +1,4 @@
+wget http://s3.amazonaws.com/open.source.geoscience/open_data/Mobil_Avo_Viking_Graben_Line_12/seismic.segy
+wget http://s3.amazonaws.com/open.source.geoscience/open_data/Mobil_Avo_Viking_Graben_Line_12/FarField.dat
+wget http://s3.amazonaws.com/open.source.geoscience/open_data/Mobil_Avo_Viking_Graben_Line_12/mobil_wellogs.tar.gz
+tar -xvzf mobil_wellogs.tar.gz
