1802e5af38250c4eeb5dee0af9d946183b629e28
mloubout <mathias.louboutin@gmail.com>
2023-06-06 21:13:58 -0400

    corner case broadcasting fixes

--- src/TimeModeling/Types/broadcasting.jl
+++ src/TimeModeling/Types/broadcasting.jl
@@ -1,18 +1,10 @@
 BroadcastStyle(::Type{<:judiMultiSourceVector}) = ArrayStyle{judiMultiSourceVector}()
 
 function similar(bc::Broadcast.Broadcasted{ArrayStyle{judiMultiSourceVector}}, ::Type{ElType}) where ElType
-    # Scan the inputs for the ArrayAndChar:
-    A = find_msv(bc)
+    # Scan the inputs
+    A = bc.args[findfirst(m -> isa(m, judiMultiSourceVector), bc.args)]
     return similar(A, ElType)
 end
-
-"`A = find_msv(As)` returns the first judiMultiSourceVector among the arguments."
-find_msv(bc::Base.Broadcast.Broadcasted) = find_msv(bc.args)
-find_msv(args::Tuple) = find_msv(find_msv(args[1]), Base.tail(args))
-find_msv(x) = x
-find_msv(::Tuple{}) = nothing
-find_msv(a::judiMultiSourceVector, rest) = a
-find_msv(::Any, rest) = find_msv(rest)
 
 extrude(x::judiMultiSourceVector) = extrude(x.data)
 
@@ -39,10 +31,10 @@ end
 
 function materialize(bc::MultiSource)
     m1, m2 = materialize(bc.m1), materialize(bc.m2)
-    ms = similar(find_msv((m1, m2)))
+    ms = similar(isa(m1, judiMultiSourceVector) ? m1 : m2)
     check_compat(m1, m2)
     for i=1:ms.nsrc
-        ms.data[i] = materialize(broadcasted(bc.op, get_src(m1, i), get_src(m2, i)))
+        ms.data[i] .= materialize(broadcasted(bc.op, get_src(m1, i), get_src(m2, i)))
     end
     ms
 end
