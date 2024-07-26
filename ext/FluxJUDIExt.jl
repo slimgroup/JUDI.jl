2111769a65a1f312eb62adb121a31418dcf6fd9e
mloubout <mathias.louboutin@gmail.com>
2024-07-26 16:13:51 -0400

    fix flux extension

--- ext/FluxJUDIExt.jl
+++ ext/FluxJUDIExt.jl
@@ -1,12 +1,9 @@
 module FluxJUDIExt
 
-import JUDI: LazyPropagation, judiVector, eval_prop
+import JUDI: LazyPropagation
 isdefined(Base, :get_extension) ? (using Flux) : (using ..Flux)
 
 Flux.cpu(x::LazyPropagation) = Flux.cpu(eval_prop(x))
 Flux.gpu(x::LazyPropagation) = Flux.gpu(eval_prop(x))
-Flux.CUDA.cu(F::LazyPropagation) = Flux.CUDA.cu(eval_prop(F))
-Flux.CUDA.cu(x::Vector{Matrix{T}}) where T = [Flux.CUDA.cu(x[i]) for i=1:length(x)]
-Flux.CUDA.cu(x::judiVector{T, Matrix{T}}) where T = judiVector{T, Flux.CUDA.CuMatrix{T}}(x.nsrc, x.geometry, Flux.CUDA.cu(x.data))
 
 end
